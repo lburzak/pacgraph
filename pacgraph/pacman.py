2994473ebecc9b587e0e0bce0cc456c67c7a92e9
polydome <polydome@tutanota.com>
2021-11-10 15:49:44 +0100

    Simplify the process of acquiring package explicitness

--- pacgraph/pacman.py
+++ pacgraph/pacman.py
@@ -2,48 +2,33 @@ import subprocess
 
 from pacgraph.models import Package
 
-FIND_INSTALLED_PACKAGES_CMD = ["expac", "%n;%m;%D"]
-FIND_EXPLICITLY_INSTALLED_PACKAGES_CMD = "pacman -Qet | awk '{print $1}'"
+FIND_INSTALLED_PACKAGES_CMD = ["expac", "-Q", "%n;%m;%D;%w"]
 RECORD_SEPARATOR = "\n"
 FIELD_SEPARATOR = ";"
 DEPS_SEPARATOR = "  "
 NAME_COLUMN = 0
 SIZE_COLUMN = 1
 DEPS_COLUMN = 2
+REASON_COLUMN = 3
 
 
 def get_installed_packages() -> [Package]:
     output = subprocess.check_output(FIND_INSTALLED_PACKAGES_CMD, universal_newlines=True)
     records = normalize_output(output).split(RECORD_SEPARATOR)
 
-    parser = PackageRecordParser()
-
-    return [parser.parse(record) for record in records]
+    return [parse_package_record(record) for record in records]
 
 
-def get_explicitly_installed_packages_names() -> [str]:
-    output = subprocess.check_output(FIND_EXPLICITLY_INSTALLED_PACKAGES_CMD, shell=True, universal_newlines=True)
-    names = normalize_output(output).split(RECORD_SEPARATOR)
+def parse_package_record(record: [str]) -> Package:
+    record = record.split(FIELD_SEPARATOR)
 
-    return names
+    return Package(
+        name=record[NAME_COLUMN],
+        size=int(record[SIZE_COLUMN]),
+        dependencies=record[DEPS_COLUMN].split(DEPS_SEPARATOR) if len(record[DEPS_COLUMN]) > 0 else [],
+        is_explicitly_installed=record[REASON_COLUMN] == 'explicit'
+    )
 
 
 def normalize_output(output):
     return output.strip('\n')
-
-
-class PackageRecordParser:
-    explicitly_installed_packages: [str]
-
-    def __init__(self) -> None:
-        self.explicitly_installed_packages = get_explicitly_installed_packages_names()
-
-    def parse(self, record: [str]) -> Package:
-        record = record.split(FIELD_SEPARATOR)
-
-        return Package(
-            name=record[NAME_COLUMN],
-            size=int(record[SIZE_COLUMN]),
-            dependencies=record[DEPS_COLUMN].split(DEPS_SEPARATOR) if len(record[DEPS_COLUMN]) > 0 else [],
-            is_explicitly_installed=record[NAME_COLUMN] in self.explicitly_installed_packages
-        )
